2db2ecbbb7fdcb6e6519cd87aecdc8e4c54ed7c6
Amal Roshan <amalazimroshan@gmail.com>
2022-05-25 10:45:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// let toggleElement = document.querySelector("#changeToggle");
+// let changeIcon = document.querySelector("#changeIcon");
+// toggleElement.addEventListener("click", () => {
+//   if (toggleElement.classList.contains("collapsed")) {
+//     changeIcon.classList += "glyphicon glyphicon-remove";
+//     console.log(true);
+//   }
+// });
